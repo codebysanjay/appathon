3866b6c37bbefccfd228febd5aa83a7a81890be0
vaishnavkkl <vaishnavkkl25@gmail.com>
2020-07-22 14:43:32 +0530

    home

--- lib/components/home_screen.dart
+++ lib/components/home_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:appathon/support/drawer.dart';
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.tealAccent,
       drawer: AppDrawer(),
+      body: Stack(children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              child: Text("welcome to disaster managment"),
+            ),
+            Container(
+                child: Column(
+              children: <Widget>[
+                Container( child:Text("")),
+                Container(),
+                Container(),
+                Container(),
+                Container(),
+              ],
+            ))
+          ],
+        ),
+      ]),
     );
   }
 }
